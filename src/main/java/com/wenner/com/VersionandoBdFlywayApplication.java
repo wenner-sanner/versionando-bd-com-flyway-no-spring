9f7b88c9c208f44b6ca23ef18f334a64cb554b02
wenner-sanner <wennersanner@hotmail.com>
2018-03-17 23:00:53 -0300

    primeiro commit

---
+++ src/main/java/com/wenner/com/VersionandoBdFlywayApplication.java
@@ -0,0 +1,12 @@
+package com.wenner.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionandoBdFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionandoBdFlywayApplication.class, args);
+	}
+}
